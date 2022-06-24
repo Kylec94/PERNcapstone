06fa5dc86e6ca00c164696c2be3d665e9b530a76
Kylec94 <kyleianc94@gmail.com>
2022-06-23 18:40:43 -0600

    third commit

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,37 +1,32 @@
 import React from "react";
 import { useEffect, useContext } from "react";
-import Scoped from "../apis/Scoped"; //importing axios baseURL fetch from Scoped api file to use here and make shorter url fetches
+import Scoped from "../apis/Scoped";
 import { RestaurantContext } from "../context/RestaurantsContext";
 import StarRating from "./StarRating";
 
 const RestaurantList = (props) => {
-  //pass props to use from context
-  const { restaurants, setRestaurants } = useContext(RestaurantContext); //destructure {} restaurants/setrestaurants from RestaurantContext context.
-
-  //   let history = useHistory("");
+  const { restaurants, setRestaurants } = useContext(RestaurantContext);
 
   useEffect(() => {
     const fetchData = async () => {
-      //useEffect has to return a function so put trycatch inside of one then async/await that function
       try {
-        const response = await Scoped.get("/"); //await fetching the '/' or 'get all' url request, then SETrestaurants to that retrieved data.
+        const response = await Scoped.get("/");
         setRestaurants(response.data.data.restaurants);
         console.log(response);
       } catch (error) {
         console.log(error);
       }
     };
-    fetchData(); //call function at end
-  }, []); //empty array to only call one time, on page load/mount
+    fetchData();
+  }, []);
 
   const handleDelete = async (e, id) => {
-    e.stopPropagation(); //pass in event object for stopPropagation which allows you to select buttons that are on a row that also has an onclick. the buttons onClick will fire without ever calling the rows onclick event
+    e.stopPropagation();
 
-    //pass in id of specific restaurant to delet
-    const response = await Scoped.delete(`/${id}`); //dynamically deleting the selected ID using template literal
+    const response = await Scoped.delete(`/${id}`);
     setRestaurants(
       restaurants.filter((restaurant) => {
-        return restaurant.id !== id; //when restaurant.id does NOT equal the id of the deleted restaurant then it is added back into the array. if it does equal then its removed from array.
+        return restaurant.id !== id;
       })
     );
   };
@@ -71,11 +66,9 @@ const RestaurantList = (props) => {
           </tr>
         </thead>
         <tbody>
-          {restaurants && //if restaurants exist then run rest of code or if not then dont
+          {restaurants &&
             restaurants.map((restaurant) => {
-              //map over each restaurant and render the jsx for each one
               return (
-                //return jsx on maps and give unique key of id. '$'.repeat(hardcode a number of times to repeat the $ OR reference restaurant.price_range for dynamic amount)
                 <tr
                   onClick={() => handleRestaurantSelect(restaurant.id)}
                   key={restaurant.id}
@@ -94,7 +87,7 @@ const RestaurantList = (props) => {
                   </td>
                   <td>
                     <button
-                      onClick={(e) => handleDelete(e, restaurant.id)} //has to be in arrow function or else it will run right away. we only want it to run when clicked
+                      onClick={(e) => handleDelete(e, restaurant.id)}
                       className="btn btn-danger"
                     >
                       Delete
