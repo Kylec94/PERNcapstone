732328605d4f1a8ff4864851a713ab100661b7d5
Kylec94 <kyleianc94@gmail.com>
2022-07-06 15:38:40 -0600

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,25 @@ import React from "react";
 const Header = () => {
   return (
     <>
-      <h1 className="font-weight-light display-1 text-center">Scoped</h1>
+      <div
+        style={{
+          color: "dark",
+          marginBottom: 30,
+          fontFamily: "Myriad Pro Regular",
+          border: "1px solid rgba(0, 0, 0, 10)",
+        }}
+        className="font-weight-light display-1 text-center"
+      >
+        Sc
+        <i
+          style={{
+            color: "linear-gradient(red, yellow, green)",
+            fontSize: "50px",
+          }}
+          class="fa-solid fa-magnifying-glass"
+        ></i>
+        ped
+      </div>
     </>
   );
 };
